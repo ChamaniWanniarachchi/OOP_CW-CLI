d2608239230f3ae23f1dc26e4de894cedf769672
ChamaniWanniarachchi <chamani.20230441@iit.ac.lk>
2024-12-03 17:34:22 +0530

    Fourth commit

---
+++ src/main/java/org/example/ConfigManager.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.json.JSONObject;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ConfigManager {
+    public void saveConfiguration (Configuration config) {
+        try (FileWriter file = new FileWriter("config.json")) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("totalTickets", config.getTotalTickets());
+            jsonObject.put("ticketReleaseDate", config.getTicketReleaseDate());
+            jsonObject.put("customerRetrievalRate", config.getCustomerRetrievalRate());
+            jsonObject.put("maxTicketCapacity", config.getMaxTicketCapacity());
+
+            file.write(jsonObject.toString());
+            System.out.println("Configuration saved successfully.");
+        } catch (IOException e) {
+            System.out.println("Error saving the configuration: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Ticketing System!");
+
+        Configuration config = new Configuration();
+        config.initialize();
+
+        System.out.println("Initialization completed successfully! Proceeding the execution...");
+    }
+}
