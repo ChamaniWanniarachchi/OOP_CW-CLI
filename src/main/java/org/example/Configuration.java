e1d280ce985bd5b7304c47beb76dc39ce6d10dc5
ChamaniWanniarachchi <chamani.20230441@iit.ac.lk>
2024-12-03 17:45:52 +0530

    Fourth commit

---
+++ src/main/java/org/example/Configuration.java
@@ -0,0 +1,83 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class Configuration {
+    private int totalTickets;
+    private LocalDate ticketReleaseDate;
+    private int customerRetrievalRate;
+    private int maxTicketCapacity;
+
+    public Configuration() {
+    }
+
+    public void initialize() {
+        Scanner scanner = new Scanner(System.in);
+
+        totalTickets = promptForPositiveInt (scanner, "Enter total number of tickets: ");
+        totalTickets = Integer.parseInt(scanner.nextLine());
+        ticketReleaseDate = promptForValidDate (scanner, "Enter ticket release date (yyyy-mm-dd): ");
+        ticketReleaseDate = LocalDate.parse(scanner.next());
+        customerRetrievalRate = promptForPositiveInt (scanner, "Enter customer retrieval rate: ");
+        customerRetrievalRate = Integer.parseInt(scanner.nextLine());
+        maxTicketCapacity = promptForPositiveInt (scanner, "Enter maximum ticket capacity: ");
+        maxTicketCapacity = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("\nConfiguration successful!");
+        System.out.println("Configuration details are as follows:");
+        System.out.println("Total tickets: " + totalTickets);
+        System.out.println("Ticket release date: " + ticketReleaseDate);
+        System.out.println("Customer retrieval rate: " + customerRetrievalRate + "ms");
+        System.out.println("Maximum ticket capacity: " + maxTicketCapacity);
+    }
+
+    private int promptForPositiveInt(Scanner scanner, String prompt) {
+        int value;
+        while (true) {
+            System.out.println(prompt);
+            if (scanner.hasNextInt()) {
+                value = scanner.nextInt();
+                if (value > 0) {
+                    return value;
+                } else {
+                    System.out.println("Value must be a positive integer. PLease try again.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter a positive integer.");
+                scanner.next();
+            }
+        }
+    }
+
+    private LocalDate promptForValidDate(Scanner scanner, String prompt) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        while (true) {
+            System.out.println(prompt);
+            String input = scanner.next();
+            try {
+                return LocalDate.parse(input, dateFormatter);
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid date format. Please enter the date in the format yyyy-mm-dd.");
+            }
+        }
+    }
+
+    public int getTotalTickets() {
+        return totalTickets;
+    }
+
+    public LocalDate getTicketReleaseDate() {
+        return ticketReleaseDate;
+    }
+
+    public int getCustomerRetrievalRate() {
+        return customerRetrievalRate;
+    }
+
+    public int getMaxTicketCapacity() {
+        return maxTicketCapacity;
+    }
+}
---
+++ src/main/java/org/example/Ticket.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Ticket {
+    private int id;
+
+    public Ticket(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
